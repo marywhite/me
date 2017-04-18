22d8417ba629b279347bea2c250ec956c90e20bc
Mary White <mary.white@icfolson.com>
2017-04-18 15:23:37 -0500

    change webpack build path so that gcloud can find static assets

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,7 @@ const path = require('path')
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve('dist'),
-    filename: 'bundle.js'
+    filename: 'src/dist/bundle.js'
   },
   module: {
     loaders: [
